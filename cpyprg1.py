d1d7de64a8c7105ee39696715b1af2b91698c143
sunil <sunilrexza@gmail.com>
2018-02-19 21:38:45 +0530

    continue

---
+++ cpyprg1.py
@@ -0,0 +1,24 @@
+from p2 import To_do_list
+
+ma=To_do_list("work")
+while True:
+	s=input("enter the choice: \n \
+		1. Add to tasks\n \
+		2. Mark Done \n \
+		3. See tasks \n \
+		4. Exit")
+
+	if s == 1:
+		ma.add()	
+
+	elif s == 2:
+		ma.mark_done()
+
+	elif s == 3:
+		ma.see_tasks()
+
+	else:
+		break
+	
+	
+	
